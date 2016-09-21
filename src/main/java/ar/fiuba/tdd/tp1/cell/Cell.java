15a7542f09d1570082e55561a8f192305774327b
juanmafc <juanmafc25@gmail.com>
2016-09-21 15:19:15 -0300

    DataCell implements getData and setData. Cell implements Observable methods

--- src/main/java/ar/fiuba/tdd/tp1/cell/Cell.java
+++ src/main/java/ar/fiuba/tdd/tp1/cell/Cell.java
@@ -1,5 +1,31 @@
 package ar.fiuba.tdd.tp1.cell;
 
+import ar.fiuba.tdd.tp1.utilities.Observable;
+import ar.fiuba.tdd.tp1.utilities.Observer;
+
+import java.util.Collection;
+
 /*  */
-public interface Cell {
+public abstract class Cell implements Observable {
+
+    Collection<Observer> observers;
+
+    @Override
+    public void registerObserver(Observer observer) {
+        this.observers.add(observer);
+    }
+
+    @Override
+    public void unregisterObserver(Observer observer) {
+        this.observers.remove(observer);
+    }
+
+    @Override
+    public void updateObservers() {
+        for (Observer observer: this.observers){
+            observer.update();
+        }
+    }
+
+
 }
--- src/main/java/ar/fiuba/tdd/tp1/cell/DataCell.java
+++ src/main/java/ar/fiuba/tdd/tp1/cell/DataCell.java
@@ -1,5 +1,17 @@
 package ar.fiuba.tdd.tp1.cell;
 
+import ar.fiuba.tdd.tp1.utilities.Observer;
+
 /*  */
-public class DataCell implements Cell {
+public class DataCell extends Cell {
+
+    private String data = " ";
+
+    public String getData(){
+        return this.data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
