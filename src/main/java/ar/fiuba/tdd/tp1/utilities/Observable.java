2036df08f548161bc7e7c26d45ef871e1fbd11cd
juanmafc <juanmafc25@gmail.com>
2016-09-21 15:15:29 -0300

    Observable and Observer methods added

--- src/main/java/ar/fiuba/tdd/tp1/utilities/Observable.java
+++ src/main/java/ar/fiuba/tdd/tp1/utilities/Observable.java
@@ -4,4 +4,7 @@ package ar.fiuba.tdd.tp1.utilities;
  * Created by juanma on 21/09/16.
  */
 public interface Observable {
+    void registerObserver(Observer observer);
+    void unregisterObserver(Observer observer);
+    void updateObservers();
 }
--- src/main/java/ar/fiuba/tdd/tp1/utilities/Observer.java
+++ src/main/java/ar/fiuba/tdd/tp1/utilities/Observer.java
@@ -3,5 +3,6 @@ package ar.fiuba.tdd.tp1.utilities;
 /**
  * Created by juanma on 21/09/16.
  */
-public class Observer {
+public interface Observer {
+    void update();
 }
