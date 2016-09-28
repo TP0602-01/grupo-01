ef9556cd504041da46ec5cdf35b8c8261952a926
diegofk26 <diegofk26@gmail.com>
2016-09-28 14:34:00 -0300

    Walk region and Walk Row tests were added
    This tests check the size and the content of the Collection returned
    bye the getCellList method

--- src/test/java/ar/fiuba/tdd/tp1/model/walk/WalkRegionTests.java
+++ src/test/java/ar/fiuba/tdd/tp1/model/walk/WalkRegionTests.java
@@ -1,7 +1,69 @@
 package ar.fiuba.tdd.tp1.model.walk;
 
-/**
- * Created by diego on 9/28/16.
- */
+
+import ar.fiuba.tdd.tp1.cell.Cell;
+import ar.fiuba.tdd.tp1.cell.InputCell;
+import ar.fiuba.tdd.tp1.gameboard.GameBoard;
+import ar.fiuba.tdd.tp1.walk.Walk;
+import ar.fiuba.tdd.tp1.walk.WalkColumn;
+import ar.fiuba.tdd.tp1.walk.WalkRegion;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class WalkRegionTests {
+    GameBoard gameBoardMock;
+    private int initialRow = 1;
+    private int initialColumn = 1;
+    private int regionSize = 2;
+    Collection<Cell> cells;
+    ArrayList<Integer> dataStored = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        gameBoardMock = mock(GameBoard.class);
+        when(gameBoardMock.getHeigth()).thenReturn(2);
+        when(gameBoardMock.getWidth()).thenReturn(2);
+        int data;
+        for (int col = initialColumn; col <= regionSize; col++) {
+            for (int row = initialRow; row <= regionSize; row++) {
+                data = col + 100 * row;
+                dataStored.add(data);
+                when(gameBoardMock.getCell(row, col)).thenReturn(new InputCell(data));
+                when(gameBoardMock.getCell(row, col)).thenReturn(new InputCell(data));
+            }
+        }
+        WalkRegion walk = new WalkRegion(gameBoardMock);
+        walk.setSizeRegion(regionSize);
+        cells = walk.getCellList(initialRow, initialColumn);
+    }
+
+
+    @Test
+    public void theSizeOfTheCellCollectionObtainedByTheGetCellListMustBeCorrect() {
+        assertEquals(regionSize * 2, cells.size());
+    }
+
+
+    @Test
+    public void theCellsReturnedHaveTheCorrectValues() {
+        boolean theDataIsContained;
+        for (Integer data : dataStored) {
+            theDataIsContained = false;
+            for (Cell cell : cells) {
+                if (data.equals(cell.getData())) {
+                    theDataIsContained = true;
+                }
+            }
+            assertTrue(theDataIsContained);
+        }
+
+    }
 }
--- src/test/java/ar/fiuba/tdd/tp1/model/walk/WalkRowTests.java
+++ src/test/java/ar/fiuba/tdd/tp1/model/walk/WalkRowTests.java
@@ -1,7 +1,61 @@
 package ar.fiuba.tdd.tp1.model.walk;
 
 
-public class WalkRowTests {
+import ar.fiuba.tdd.tp1.cell.Cell;
+import ar.fiuba.tdd.tp1.cell.InputCell;
+import ar.fiuba.tdd.tp1.gameboard.GameBoard;
+import ar.fiuba.tdd.tp1.walk.Walk;
+import ar.fiuba.tdd.tp1.walk.WalkColumn;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class WalkRowTests {
+    GameBoard gameBoardMock;
+    private int initialRow = 1;
+    private int initialColumn = 1;
+    private int rowSize = 2;
+    Collection<Cell> cells;
+    ArrayList<Integer> dataStored = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        gameBoardMock = mock(GameBoard.class);
+        when(gameBoardMock.getHeigth()).thenReturn(2);
+        for (int row = initialRow; row <= rowSize; row++) {
+            dataStored.add(row);
+            when(gameBoardMock.getCell(row, initialColumn)).thenReturn(new InputCell(row));
+            when(gameBoardMock.getCell(row, initialColumn)).thenReturn(new InputCell(row));
+        }
+        Walk walk = new WalkColumn(gameBoardMock);
+        cells = walk.getCellList(initialRow, initialColumn);
+    }
+
+    @Test
+    public void theAmountOfCellsReturnedByTheGetCellListMethodIsCorrect() {
+        assertEquals(rowSize, cells.size());
+    }
+
+    @Test
+    public void theCellsReturnedHaveTheCorrectValues() {
+        boolean theDataIsContained;
+        for (Integer data : dataStored) {
+            theDataIsContained = false;
+            for (Cell cell : cells) {
+                if (data.equals(cell.getData())) {
+                    theDataIsContained = true;
+                }
+            }
+            assertTrue(theDataIsContained);
+        }
+
+    }
 
 }
