c34c3be90bc7caa0cd53b56d8135f8c328ce1819
diegofk26 <diegofk26@gmail.com>
2016-09-28 13:33:44 -0300

    Add mockito library and WalkColumnTests

--- src/test/java/ar/fiuba/tdd/tp1/model/walk/WalkColumnTest.java
+++ src/test/java/ar/fiuba/tdd/tp1/model/walk/WalkColumnTest.java
@@ -1,7 +1,50 @@
 package ar.fiuba.tdd.tp1.model.walk;
+import ar.fiuba.tdd.tp1.cell.Cell;
+import ar.fiuba.tdd.tp1.cell.InputCell;
+import ar.fiuba.tdd.tp1.gameboard.GameBoard;
+import ar.fiuba.tdd.tp1.walk.Walk;
+import ar.fiuba.tdd.tp1.walk.WalkColumn;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-/**
- * Created by diego on 9/28/16.
- */
+import java.util.Collection;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.*;
+
+
 public class WalkColumnTest {
+    GameBoard gameBoardMock;
+    private int initialRow= 1;
+    private int initialColumn = 1;
+    private int columnSize = 2;
+    Collection<Cell> cells;
+
+    @Before
+    public void setUp(){
+        gameBoardMock = mock(GameBoard.class);
+        when(gameBoardMock.getHeigth()).thenReturn(2);
+        for(int col = initialColumn; col <= columnSize; col++){
+
+            when(gameBoardMock.getCell(initialRow,col)).thenReturn(new InputCell(col));
+            when(gameBoardMock.getCell(initialRow,col)).thenReturn(new InputCell(col));
+        }
+        Walk walk = new WalkColumn(gameBoardMock);
+        cells = walk.getCellList(initialRow,initialColumn);
+    }
+
+    @Test
+    public void theAmountOfCellsReturnedByTheGetCellListMethodIsCorrect(){
+        assertEquals(columnSize,cells.size());
+    }
+
+    @Test
+    public void theCellsReturnedHaveTheCorrectValues(){
+        int data = initialColumn;
+        for( Cell cell: cells) {
+            assertEquals(data, (int)cell.getData());
+            data++;
+        }
+    }
 }
