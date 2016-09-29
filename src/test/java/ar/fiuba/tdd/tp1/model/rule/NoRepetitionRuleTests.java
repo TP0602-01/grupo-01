323a2e2f6ea8408889d288e8329d75b1750af972
diegofk26 <diegofk26@gmail.com>
2016-09-29 11:45:33 -0300

    Basic test for the class NoRepetitionRule were added

--- src/test/java/ar/fiuba/tdd/tp1/model/rule/NoRepetitionRuleTests.java
+++ src/test/java/ar/fiuba/tdd/tp1/model/rule/NoRepetitionRuleTests.java
@@ -1,42 +1,39 @@
 package ar.fiuba.tdd.tp1.model.rule;
 
+
+import ar.fiuba.tdd.tp1.rule.NoRepetitionRule;
 import ar.fiuba.tdd.tp1.rule.SumRule;
 import ar.fiuba.tdd.tp1.walk.Walk;
-
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
-
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
+public class NoRepetitionRuleTests {
 
-public class SumRuleTests {
-
-    SumRule sumRule;
+    NoRepetitionRule noRepetitionRule;
     RuleTestUtilities utilities = new RuleTestUtilities();
 
-
     @Test
-    public void theCheckMethodMustReturnTrueWhenTheSumIsCorrect(){
-        String[][] cellData = {{"1" ,"2"}};
-        Walk walkMock = utilities.createAWalkMock(0,0,cellData);
-        Collection<String> cellAsString = new ArrayList<>();
-        cellAsString.add("0,0");
-        sumRule = new SumRule(cellAsString,walkMock,3);
-        assertTrue(sumRule.check());
-    }
-
-    @Test
-    public void theCheckMethodMustReturnFalseIfTheSumValueIsDifferentToTheCompareValue(){
+    public void checkingTheRuleInACollectionOfCellsThatDoNotHaveRepeatedElementsMustReturnTrue(){
         String[][] cellData = {{"1" ,"2","3"}};
         Walk walkMock = utilities.createAWalkMock(0,0,cellData);
         Collection<String> cellAsString = new ArrayList<>();
         cellAsString.add("0,0");
-        sumRule = new SumRule(cellAsString,walkMock,1);
-        assertFalse(sumRule.check());
+        noRepetitionRule = new NoRepetitionRule(cellAsString,walkMock);
+        assertTrue(noRepetitionRule.check());
     }
 
+    @Test
+    public void ifThereAreRepeatedElementsCheckMustReturnFalse(){
+        String[][] cellData = {{"1" ,"2","1"}};
+        Walk walkMock = utilities.createAWalkMock(0,0,cellData);
+        Collection<String> cellAsString = new ArrayList<>();
+        cellAsString.add("0,0");
+        noRepetitionRule = new NoRepetitionRule(cellAsString,walkMock);
+        assertFalse(noRepetitionRule.check());
+    }
 }
---
+++ src/test/java/ar/fiuba/tdd/tp1/model/rule/RuleTestUtilities.java
@@ -0,0 +1,26 @@
+package ar.fiuba.tdd.tp1.model.rule;
+
+
+import ar.fiuba.tdd.tp1.cell.Cell;
+import ar.fiuba.tdd.tp1.cell.InputCell;
+import ar.fiuba.tdd.tp1.walk.Walk;
+
+import java.util.Vector;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RuleTestUtilities {
+
+    public Walk createAWalkMock(int initialRow, int initialColumn, String[][] cellData){
+        Walk walkMock = mock(Walk.class);
+        Vector<Cell> collectionReturnedByWalk = new Vector<>();
+        for(int row = 0; row < cellData.length ; row++){
+            for (int column = 0 ; column < cellData[row].length; column++)
+                collectionReturnedByWalk.add(new InputCell(cellData[row][column]));
+        }
+        when(walkMock.getCellList(initialRow,initialColumn)).thenReturn(collectionReturnedByWalk);
+        return walkMock;
+    }
+
+}
