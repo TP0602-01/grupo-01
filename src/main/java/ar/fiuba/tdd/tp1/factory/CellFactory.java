56addf12eaab15220856972bf960be3b7307e4b8
diegofk26 <diegofk26@gmail.com>
2016-09-29 16:04:51 -0300

    CellFactory now returns a NullCell instead of FixedCell(null)

--- src/main/java/ar/fiuba/tdd/tp1/factory/CellFactory.java
+++ src/main/java/ar/fiuba/tdd/tp1/factory/CellFactory.java
@@ -3,6 +3,7 @@ package ar.fiuba.tdd.tp1.factory;
 import ar.fiuba.tdd.tp1.cell.Cell;
 import ar.fiuba.tdd.tp1.cell.FixedCell;
 import ar.fiuba.tdd.tp1.cell.InputCell;
+import ar.fiuba.tdd.tp1.cell.NullCell;
 
 /* */
 public class CellFactory {
@@ -11,11 +12,12 @@ public class CellFactory {
         if (type.equals(Cell.DATA_TYPE)) {
             return new InputCell("0");
         } else if (type.equals(Cell.NULL_TYPE)) {
-            return new FixedCell(null);
+            return new NullCell();
         } else if (type.equals(Cell.KAKORU_TYPE)) {
             return new FixedCell(content);
         } else {
             return null;
+
         }
     }
 }
