8caa50f86d16e2814684520b199856bc86cd7df6
juanmafc <juanmafc25@gmail.com>
2016-09-29 21:35:16 -0300

    Integrating configuration with previously loaded model

--- src/main/java/ar/fiuba/tdd/tp1/factory/RuleFactory.java
+++ src/main/java/ar/fiuba/tdd/tp1/factory/RuleFactory.java
@@ -10,19 +10,24 @@ public class RuleFactory {
 
     public static final String NO_REPETITION_TYPE = "no_rep";
     public static final String SUM_TYPE = "sum";
+    public static final String MUL_TYPE = "mul";
     public static final String DELIMITER = "_";
 
     public static BaseRule create(String type, Walk walkObject, Collection<String> cellPositions) {
         if (type.equals(NO_REPETITION_TYPE)) {
             return new NoRepetitionRule(cellPositions, walkObject);
-        } else if (type.equals(SUM_TYPE)) {
+        } else {
             String initCells = (cellPositions.iterator()).next();
 
             cellPositions.clear();
             cellPositions.add(initCells.split(DELIMITER)[0]);
             int sumNumber = Integer.parseInt(initCells.split(DELIMITER)[1]);
 
-            return new SumRule(cellPositions, walkObject, sumNumber);
+            if (type.equals(SUM_TYPE)) {
+                return new SumRule(cellPositions, walkObject, sumNumber);
+            } else if (type.equals(MUL_TYPE)) {
+                return new MulRule(cellPositions, walkObject, sumNumber);
+            }
         }
         return null;
     }
