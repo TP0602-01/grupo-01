1a3fa23dd999c1b212a2a652c379cc5f0c34c524
juanmafc <juanmafc25@gmail.com>
2016-10-09 11:53:37 -0300

    ConcreteSquareLinker Refactor now uses LinkngTable
    This makes it more understandable

--- src/main/java/ar/fiuba/tdd/tp1/graph/linker/ConcreteSquareLinker.java
+++ src/main/java/ar/fiuba/tdd/tp1/graph/linker/ConcreteSquareLinker.java
@@ -14,85 +14,54 @@ import java.util.Set;
 public class ConcreteSquareLinker implements SquareLinker {
 
     //TODO: se podrian crear objetos para que esto quede mas entendible
-    //TODO: en definitiva es MAP<key=[rowOffset, colOffset], value=[ TOKEN_ORIGEN, [TOKEN_DESTINO1,..., TOKEN_DESTINOn] ]>
-    Map<Pair<Integer,Integer>, Map<String, Set<String>> > linkingInfo;
-
     LinkeableMatrix linkeableMatrix;
     LinksManager linksManager;
+    LinkingTable linkingTable;
 
 
 
 
     public ConcreteSquareLinker(LinkeableMatrix linkeableMatrix, LinksManager linksManager){
-        this.linkingInfo = new HashMap<>();
         this.linkeableMatrix = linkeableMatrix;
         this.linksManager = linksManager;
+        this.linkingTable = new LinkingTable();
     }
 
 
     @Override
     public void setLinkingInfo(int rowOffset, int columnOffset, String originTokens, Set<String> destinationTokens) {
         //Setea en que direccion (representados como offsets) se van a chequear los linkeos
-
-        Pair<Integer, Integer> directionOffset = new Pair<>(rowOffset,columnOffset);
-        Map<String, Set<String>> linkeableTokensInChosenOffset;
-        if ( this.linkingInfo.containsKey(directionOffset) ){
-            linkeableTokensInChosenOffset = this.linkingInfo.get(directionOffset);
+        for( String destinationToken: destinationTokens){
+            this.linkingTable.addEntry(rowOffset, columnOffset, originTokens, destinationToken);
         }
-        else {
-            linkeableTokensInChosenOffset = new HashMap<>();
-        }
-        linkeableTokensInChosenOffset.put(originTokens, destinationTokens);
-        this.linkingInfo.put( directionOffset, linkeableTokensInChosenOffset);
-
     }
 
     @Override
     public void updateLinkeablesLinks(int row, int column) {
 
+
         Linkeable origin = this.linkeableMatrix.getLinkeable(row, column);
         Set<String> originLinkingTokens = origin.getLinkingTokens();
-
         //Recorro cada posible offset de la tabla
+        for ( Pair<Integer, Integer> currentOffset: this.linkingTable.getOffsets() ) {
+            boolean shouldBeLinked = false;
+            int rowOffset = currentOffset.getKey();
+            int columnOffset = currentOffset.getValue();
 
-        //TODO: podria hacerse mas abstracto
-        for (Map.Entry<Pair<Integer,Integer>, Map<String, Set<String>>> currentInfoEntry : this.linkingInfo.entrySet()){
-            //currentInfoEntry tiene key=[rowOffset, colOffset], value=[ TOKEN_ORIGEN, [TOKEN_DESTINO1,..., TOKEN_DESTINOn] ]
-
-            int rowOffset = currentInfoEntry.getKey().getKey();//RIP DEMETER LAW
-            int columnOffset = currentInfoEntry.getKey().getValue();//RIP DEMETER LAW
             Linkeable destination = this.linkeableMatrix.getLinkeable(row + rowOffset, column + columnOffset);
-
-
             if (destination != null) {
-                boolean shouldBeLinked = false;
+                Set<String> destinationLinkingTokens = destination.getLinkingTokens();
 
-
-                //Tengo que acceder a la tabla de linkeo.
-                Map<String, Set<String>> currentLinkeableTokensInCurrentOffset = currentInfoEntry.getValue();
-
-
-
-                for (String originToken: originLinkingTokens){
-
-                    if ( currentLinkeableTokensInCurrentOffset.containsKey(originToken) ){
-
-
-                        Set<String> compatibleDestinationTokensInCurrentOffset = currentLinkeableTokensInCurrentOffset.get(originToken);
-                        Set<String> destinationLinkingTokens = destination.getLinkingTokens();
-
-                        for (String adyacentToken: destinationLinkingTokens){
-                            if (compatibleDestinationTokensInCurrentOffset.contains(adyacentToken)){
-                                shouldBeLinked = true;
-                            }
+                for (String originToken : originLinkingTokens) {
+                    for (String destinationToken : destinationLinkingTokens) {
+                        if (this.linkingTable.checkEntryExistance(rowOffset, columnOffset, originToken, destinationToken)) {
+                            shouldBeLinked = true;
                         }
                     }
                 }
-
-                if (shouldBeLinked){
+                if (shouldBeLinked) {
                     this.linksManager.addNotDirectedLinkBetween(origin, destination);
-                }
-                else{
+                } else {
                     this.linksManager.removeNotDirectedLinkBetween(origin, destination);
                 }
             }
