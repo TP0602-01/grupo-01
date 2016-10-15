61ec376696dc9e008310ba8ca83376e9783de4b9
juanmafc <juanmafc25@gmail.com>
2016-10-15 19:43:37 -0300

    Unnecesary imports deleted

--- src/main/java/ar/fiuba/tdd/tp1/rule/NoRepetitionRule.java
+++ src/main/java/ar/fiuba/tdd/tp1/rule/NoRepetitionRule.java
@@ -2,9 +2,6 @@ package ar.fiuba.tdd.tp1.rule;
 
 import ar.fiuba.tdd.tp1.cell.Cell;
 import ar.fiuba.tdd.tp1.graph.Graph;
-import com.sun.deploy.util.StringUtils;
-import javafx.util.converter.IntegerStringConverter;
-import javafx.util.converter.NumberStringConverter;
 
 import java.util.Collection;
 import java.util.HashSet;
