f12edb7faa73bf64bb993ababb6237170487b8d3
juanmafc <juanmafc25@gmail.com>
2016-10-20 14:16:38 -0300

    ConnectedGraphsCountRule added
    This will be needed for Slitherlink

---
+++ src/main/java/ar/fiuba/tdd/tp1/rule/utilities/ConnectedGraphsCountRule.java
@@ -0,0 +1,20 @@
+package ar.fiuba.tdd.tp1.rule.utilities;
+
+import ar.fiuba.tdd.tp1.graph.Graph;
+import ar.fiuba.tdd.tp1.rule.Rule;
+
+public class ConnectedGraphsCountRule extends Rule {
+
+
+    private int expectedCount;
+
+    public ConnectedGraphsCountRule(int expectedCount) {
+        this.expectedCount = expectedCount;
+    }
+
+
+    @Override
+    public boolean check(Graph graph) {
+        return graph.getConnectedSubGraphsCount() == this.expectedCount;
+    }
+}
