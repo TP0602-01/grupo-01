7170e91825535dad6f0ba57618daa97de6ad9c37
juanmafc <juanmafc25@gmail.com>
2016-10-20 04:51:07 -0300

    ExistCircuitRule check() modified to use quantity
    and prevent pmd and findbugs errors

--- src/main/java/ar/fiuba/tdd/tp1/rule/ExistCircuitRule.java
+++ src/main/java/ar/fiuba/tdd/tp1/rule/ExistCircuitRule.java
@@ -4,14 +4,22 @@ import ar.fiuba.tdd.tp1.graph.Graph;
 
 public class ExistCircuitRule {
     Graph mygraph;
-    //int quantityCheck;
+    int quantityCheck;
 
     ExistCircuitRule(Graph graph, int quantity) {
         this.mygraph = graph;
-        //this.quantityCheck = quantity;
+        this.quantityCheck = quantity;
     }
 
     boolean check() {
+        //TODO: VER ESTO SI SE CAMBIA A DEVOLVER LA CANTIDAD DE CIRCUITOS
+        if ( (this.quantityCheck == 0) && (!mygraph.getCircuitCount()) ) {
+            return false;
+        }
+        else if ( (this.quantityCheck >= 1) && (mygraph.getCircuitCount()) ) {
+            return true;
+        }
         return mygraph.getCircuitCount();
+
     }
 }
