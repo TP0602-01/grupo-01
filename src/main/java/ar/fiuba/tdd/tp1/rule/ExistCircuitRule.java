cd637a32ded1fa3ead4c3cbb558fb0c741b05b8c
juanmafc <juanmafc25@gmail.com>
2016-11-03 16:45:51 -0300

    Unnecessary variable removed

--- src/main/java/ar/fiuba/tdd/tp1/rule/ExistCircuitRule.java
+++ src/main/java/ar/fiuba/tdd/tp1/rule/ExistCircuitRule.java
@@ -3,11 +3,9 @@ package ar.fiuba.tdd.tp1.rule;
 import ar.fiuba.tdd.tp1.graph.Graph;
 
 public class ExistCircuitRule extends Rule {
-    Graph mygraph;
     int quantityCheck;
 
     public ExistCircuitRule(Graph graph, int quantity) {
-        this.mygraph = graph;
         this.quantityCheck = quantity;
     }
 
