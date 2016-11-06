a7b6c23d1bdf779539e9536d32dcba5b09db3120
juanmafc <juanmafc25@gmail.com>
2016-11-06 20:54:28 -0300

    IndexedGraph test added

--- src/test/java/ar/fiuba/tdd/tp1/graph/SubgraphTests.java
+++ src/test/java/ar/fiuba/tdd/tp1/graph/SubgraphTests.java
@@ -1,13 +1,12 @@
 package ar.fiuba.tdd.tp1.graph;
 
 import ar.fiuba.tdd.tp1.cell.Cell;
+import ar.fiuba.tdd.tp1.cell.InputCell;
 import ar.fiuba.tdd.tp1.gameboard.GameBoard;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -17,6 +16,30 @@ import static org.junit.Assert.assertTrue;
  * Created by User on 06/11/2016.
  */
 public class SubgraphTests {
+
+    @Test
+    public void testIndexedGraphIndexesCellsInTheSameOrderOfThePassedCollection() {
+        Cell a0 = new InputCell("");
+        Cell a1 = new InputCell("");
+        Cell a2 = new InputCell("");
+
+        Collection<Cell> cells = new HashSet<>();
+        cells.add(a2);
+        cells.add(a0);
+        cells.add(a1);
+
+        IndexedGraph indexedGraph = new IndexedGraph(cells, new Graph());
+        Collection<Cell> indexedGraphCells = indexedGraph.getCells();
+
+        Iterator<Cell> expectedCell = cells.iterator();
+        Iterator<Cell> actualCell = indexedGraphCells.iterator();
+
+        int i = 0;
+        while (i < 3) {
+            assertEquals(expectedCell.next(), actualCell.next());
+            i++;
+        }
+    }
 
     @Test
     public void testGettingASubgraphShouldReturnAGraphContainningOnlyThoseLinksBetweenPassedCells() {
