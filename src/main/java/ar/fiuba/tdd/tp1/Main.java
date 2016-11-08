c096a6e2afa19df9407a6292ce96a4534cff0737
eze210 <eze210@gmail.com>
2016-11-08 01:05:52 -0300

    Passing a parameter to choose the game. By default, the game is a country_road. #22

--- src/main/java/ar/fiuba/tdd/tp1/Main.java
+++ src/main/java/ar/fiuba/tdd/tp1/Main.java
@@ -9,10 +9,22 @@ import ar.fiuba.tdd.tp1.view.BoardView;
 
 public class Main {
 
+    private static final String getFilesPath(String []args) {
+        String folder = "./src/main/java/ar/fiuba/tdd/tp1/game_files/";
+
+        if (args.length == 0) {
+            /* default value */
+            return folder + "country_road";
+        } else {
+            /* value passed in command line */
+            return folder + args[0];
+        }
+    }
+
     public static void main(String[] args) {
 
         try {
-            String filesPath = "./src/main/java/ar/fiuba/tdd/tp1/game_files/country_road";
+            String filesPath = getFilesPath(args);
             GameParser parser = new GameParser(filesPath);
 
             String filePlaysOutput = "./src/main/java/ar/fiuba/tdd/tp1/game_files/output.json";
